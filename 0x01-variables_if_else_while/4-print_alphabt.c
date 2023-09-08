97b9ed73fc01c86e8942807fe9b213a61d8b2606
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-08 10:06:39 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints the alphabet except q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
