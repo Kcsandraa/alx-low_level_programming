226a0a80e3c5b822e74d33b5d4793247cc8f3282
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-08 12:55:00 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num, num1, num2;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num1 = num + 1; num1 < 10; num1++)
+		{
+			for (num2 = num1 + 1; num2 < 10; num2++)
+			{
+				putchar(num + '0');
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+
+				if (num == 7 && num1 == 8 && num2 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
