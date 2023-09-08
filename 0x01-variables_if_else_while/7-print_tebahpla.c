d504e740c14d05f640806ad8d2ece3a1ae1a2afa
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-08 10:46:55 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
