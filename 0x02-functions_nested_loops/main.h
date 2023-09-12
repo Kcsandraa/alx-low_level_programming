e7698462ba159898ca8dab87a5c14ac1779354c7
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-12 10:38:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,13 +12,13 @@ int print_sign(int n);
 
 int _abs(int n);
 
-int print_last_digit(int);
+int print_last_digit(int n);
 
 void jack_bauer(void);
 
 void times_table(void);
 
-int add(int, int);
+int add(int i, int n);
 
 void print_to_98(int n);
 
