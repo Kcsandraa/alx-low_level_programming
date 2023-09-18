ae3e50d446ecc854553043dec1522cb7acf1a48e
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-18 16:47:09 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,34 @@
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted to an integer
- * Return: int conveter
+ * _atoi - Convert a string to integer
+ * @s: char array string
+ * Return: integer
  */
 int _atoi(char *s)
 {
 	int len, count, sign;
 
 	count = 0;
-	sign = 1;
-	len = 0;
+	sign = -1;
 
-	while (s[len] == ' ' || (s[len] >= 9 && s[len] <= 13))
-	{
-		len++;
-	}
-
-	while (s[len] == '-' || s[len] == '+')
+	for (len = 0; s[len] != '\0'; len++)
 	{
 		if (s[len] == '-')
 			sign *= -1;
-		len++;
-	}
 
-	while (s[len] != '\0')
-	{
-		if (s[len] >= '0' && s[len] <= '9')
+		if (s[len] > 47 && s[len] < 58)
 		{
-			count = count * 10 + (s[len] - '0');
+			if (count < 0)
+				count = (count * 10) - (s[len] - '0');
+			else
+				count = (s[len] - '0') * -1;
+
+			if (s[len + 1] < 48 || s[len + 1] > 57)
+				break;
 		}
-		else if (count != 0)
-		{
-			break;
-		}
-		len++;
 	}
-	return (sign * count);
+	if (sign < 0)
+		count *= -1;
+
+	return (count);
 }
