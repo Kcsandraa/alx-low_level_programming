4e715aa17b1ca7f689ffe792a0086caf825895e6
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-21 07:06:19 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: parameter
+ * @src: parameter
+ *
+ * Return:  a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j, len = 0;
+
+	while (dest[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[len + j] = src[j];
+	}
+	dest[len + j] = '\0';
+	return (dest);
+}
