b3206109b2634d0dff26dca319f9105f0c2ce793
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-09-25 06:05:05 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] ==  c)
+		if (*s == c)
 			return (s);
-		else if (s[i + 1] == c)
+		else if (*(s + 1) == c)
 			return (s + 1);
-		i++;
+		s++;
 	}
 	return (s + 1);
 }
