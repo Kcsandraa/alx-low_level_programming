fc92255dbb0b5f539cd5899571f17211dd2934f8
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-10-05 12:52:24 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned char *ptr;
 
 	if (nmemb == 0 || size == 0)
-	       return (NULL);
+		return (NULL);
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
