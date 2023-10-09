720f52496972c864fb6082814d71c3c1f995ecc1
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-10-09 14:46:03 -0700

    new_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,7 @@ typedef struct dog
 #define FUNCTIONS
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+char *_strdup(char *str);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
