b068150f01111dab33d7ee1fe01c011396288a62
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-10-09 11:33:27 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - description of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char c);
+
+#endif
