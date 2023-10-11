3301bb28d261ae55a320aaf2b9272b0327db7131
kcsandraa <kcsandra@students.uonbi.ac.ke>
2023-10-11 04:34:20 -0700

    callback using an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * array_iterator - executes a function 
+ * array_iterator - executes a function
  * given as a parameter on each element of an array
  * @array: The array pointer
  * @size: size of the array
@@ -9,7 +9,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (array == NULL || action == NULL)
